4e44b1ff85817f7064d254208ca760f93cc5bc0a
WU Jun <quark@lihdd.net>
2012-10-19 20:21:48 +0800

    make session cookies lifetime longer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Shorten cookie key to make incoming traffic smaller
 
-RailsNew::Application.config.session_store :active_record_store, :key => 's'
+RailsNew::Application.config.session_store :active_record_store, :key => 's', expire_after: 2.years
