a834c7e3cb040d74e12e79d27ec036de8fbc1b91
WU Jun <quark@lihdd.net>
2012-10-26 02:24:36 +0800

    bullet should not mangle html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,10 +42,10 @@ RailsNew::Application.configure do
   config.after_initialize do
     next unless defined?(Bullet)
     Bullet.enable = true
-    Bullet.alert = true
+    Bullet.alert = false
     Bullet.bullet_logger = true
     Bullet.console = true
-    Bullet.rails_logger = true
+    # Bullet.rails_logger = true
     Bullet.disable_browser_cache = false
   end
 end
